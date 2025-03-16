dacc74d9d798992939d39212982c4bfb15620a15
Jagadish <adhikari.jagdish5200@gmail.com>
2025-03-16 06:45:41 +0545

    First activity added

--- app/src/main/java/com/texas/bcasixthtexas/MainActivity.java
+++ app/src/main/java/com/texas/bcasixthtexas/MainActivity.java
@@ -1,6 +1,9 @@
 package com.texas.bcasixthtexas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSubmit = findViewById(R.id.btn_main_submit);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
