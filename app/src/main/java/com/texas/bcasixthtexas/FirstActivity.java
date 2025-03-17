8944d670e43e681114c323dd28ea933052fcbcf2
Jagadish <adhikari.jagdish5200@gmail.com>
2025-03-17 06:40:57 +0545

    Minor changes

--- app/src/main/java/com/texas/bcasixthtexas/FirstActivity.java
+++ app/src/main/java/com/texas/bcasixthtexas/FirstActivity.java
@@ -1,6 +1,8 @@
 package com.texas.bcasixthtexas;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FirstActivity extends AppCompatActivity {
+
+    List<String> items = Arrays.asList("Kathmandu", "Chandragiri", "Nagarkot", "Pokhara", "Lumbini");
+    Spinner afSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        afSpinner = findViewById(R.id.af_spinner);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>
+                (getApplicationContext(), android.R.layout.simple_spinner_item, items);
+        afSpinner.setAdapter(adapter);
     }
 }
