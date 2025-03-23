c08d93a33351115109a6a76b8f405f2a9a86c411
Jagadish <adhikari.jagdish5200@gmail.com>
2025-03-23 06:41:50 +0545

    Fragment started

--- app/src/main/java/com/texas/bcasixthtexas/GridViewActivity.java
+++ app/src/main/java/com/texas/bcasixthtexas/GridViewActivity.java
@@ -1,14 +1,21 @@
 package com.texas.bcasixthtexas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class GridViewActivity extends AppCompatActivity {
+
+    Button agvBtnListView, agvBtnGridView;
+    FrameLayout agvFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,33 @@ public class GridViewActivity extends AppCompatActivity {
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_grid_view);
 
+        agvBtnGridView = findViewById(R.id.agv_btn_gridView);
+        agvBtnListView = findViewById(R.id.agv_btn_listview);
+        agvFrameLayout = findViewById(R.id.agv_frameLayout);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        agvBtnListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        agvBtnGridView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment currentFragment){
+
+    }
+
 }
